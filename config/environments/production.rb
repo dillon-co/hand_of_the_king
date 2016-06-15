065b1f20c32896b4c5b708f09ee315c8adaf6fec
grandmasterspoc <dilloncortez@gmail.com>
2016-06-14 19:12:06 -0600

    fixed typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   s3_credentials: {
     bucket: ENV['AWS_S3_BUCKET'],
     access_key_id: ENV['AMAZON_ACCESS_KEY_ID'],
-    secret_access_key: ENV['AMAZON_SECRET_ACCESS_KEY']
+    secret_access_key: ENV['AMAZON_SECRET_ACCESS_KEY'],
     s3_region: 'US Standard'
   }
 }
