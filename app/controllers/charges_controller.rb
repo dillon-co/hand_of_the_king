02b991d346fce7ea9e96e1c72640a7f458aed14f
grandmasterspoc <dilloncortez@gmail.com>
2016-08-11 19:58:30 -0600

    finished stripe integration

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,16 +1,17 @@
 class ChargesController < ApplicationController
 
 def new
+
 end
 
 def create
    # Amount in cents
-  @amount = 1997
+  @amount = params[:amount]
   token = params[:stripeToken]
 
   current_credits = current_user.credits
 
-  current_user.update(credits: current_credits+3)
+  current_user.update(credits: current_credits+params[:credits].to_i)
 
   customer = Stripe::Customer.create(
     card: token,
@@ -23,6 +24,8 @@ def create
     :description => 'Apply',
     :currency    => 'usd'
   )
+
+redirect_to edit_job_link_path(current_user.job_links.last)
 
 rescue Stripe::CardError => e
   flash[:error] = e.message
