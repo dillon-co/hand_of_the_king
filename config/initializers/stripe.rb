152e9000d6b9c2cd1068eb5d269de8952e32ad01
grandmasterspoc <dilloncortez@gmail.com>
2016-08-22 00:32:38 -0600

    added stripe key env variables

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => 'pk_test_jcjMPgneaF1NtY3KYVWWXNNc',
-  :secret_key      => 'sk_test_JJUC9VeCStYpevtDBX9p4UmP'
+  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+  :secret_key      => ENV['STRIPE_SECRET_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
