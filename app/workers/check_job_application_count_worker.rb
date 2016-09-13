f9825c40a6e11b86af72f52a40bb6552fd41187f
grandmasterspoc <dilloncortez@gmail.com>
2016-09-12 22:34:36 -0600

    added worder for stats

--- app/workers/check_job_application_count_worker.rb
+++ app/workers/check_job_application_count_worker.rb
@@ -2,6 +2,6 @@ class JobApplicationCountWorker
   include Sidekiq::Worker
 
   def perform
-    puts User.all.map{ |u| u.job_links.present? ? u.job_links.all.map{ |j| if j.present? ? j.job_applications.where(applied_to: true).count }.inject(&:+) : 0 }.inject(:+)
+    puts User.all.map{ |u| u.job_links.present? ? u.job_links.all.map{ |j| j.job_applications.where(applied_to: true).count }.inject(&:+) : 0 }.inject(:+)
   end
 end    
