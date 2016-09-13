33838ae4f855b34858a667bbc4528614ff02ed97
grandmasterspoc <dilloncortez@gmail.com>
2016-09-13 01:18:11 -0600

    updated stats

--- app/workers/check_job_application_count_worker.rb
+++ app/workers/check_job_application_count_worker.rb
@@ -5,7 +5,7 @@ class JobApplicationCountWorker
     # puts User.all.map{ |u| u.job_links.present? ? u.job_links.all.map{ |j| j.job_applications.where(applied_to: true).count }.inject(&:+) : 0 }.inject(:+)
     users = ["Dillon", "Alexander", "mike", "Karson", "Tim", "Ben", "Haley", "Coleman", "Brendan"]
     new_users = Hash.new do |h, k| 
-      h[k] = User.find_by(first_name: k).job_links.map { |j| j.job_applications.where(applied_to: true).count }
+      h[k] = User.find_by(first_name: k).job_links.map { |j| j.job_applications.where(applied_to: true).count }.inject(:+)
     end
     users.each do |us|
       new_users[us]
